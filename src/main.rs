6a632881cfb699de04130f1f4a246a5b0813bf4b
Anton Sapunov <fort.sav.28@gmail.com>
2019-04-19 01:29:01 +0700

    With iterators

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,13 @@ use minigrep;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {}", err);
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
